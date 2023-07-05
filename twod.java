64a3fdfcde4c7cd6cdfcf3ace32263ca8017fb89
amazingak1 <137612747+amazingak1@users.noreply.github.com>
2023-07-05 23:44:38 +0530

    twod.java

--- twod.java
+++ twod.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
-
+//I have named my file 'twod'||you can name anything:)
 public class twod {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
+        //taking input of row and columns 
         System.out.println("Enter row and coloums");
         int row =sc.nextInt();
         int colm=sc.nextInt();
 
+        /*this is called two dimestion array,
+        this is like a matrix
+            1 2 3
+            3 2 1 
+            this was a example of 2x3 matrix */
         int marks[][]=new int[row][colm];
-        
+
+        //taking all values
         System.out.println("Enter the values");
         for(int i=0;i<row;i++){
             System.out.println("New Row");
@@ -19,7 +26,7 @@ public class twod {
         }
         
         System.out.println("Matrix");
-
+    //printing all values in order
         for(int i=0;i<row;i++){
             for(int j=0;j<colm;j++){
                 System.out.print(marks[i][j]+" ");
@@ -27,12 +34,14 @@ public class twod {
          System.out.println();
         }
 
+    //searching a specific value given by user, and showing its row and column number.
         System.out.println("Search for: ");
         byte search = sc.nextByte();
         sc.close();
         for(int i=0;i<row;i++){
             for(int j=0;j<colm;j++){
                 if(marks[i][j]==search){
+    //in java count start from zero, and we also took took i and j from 1, if a number we are searching is in row 1 it will show 0 so we added +1 in row number and coloum number
                     System.out.println(search+"is at Row number: "+(i+1)+" Coloum number: "+(j+1));
                 }
             }
